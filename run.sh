404142d7bacdc07ee6dcc6d5a2e0e05448758a51
Geraint White <geraint@android.net>
2014-04-06 22:40:10 +0100

    Added support for github deploy

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+url="$1"
+name="$2"
 path="/home/git/post-receive/processing"
 
 shopt -s extglob
 rm -rf $path/!(node_modules)
 
+cd /home/git/$name.git
+
+git fetch $url master:master
 export GIT_WORK_TREE=$path
 git checkout -f
 

3ed31b90d556800c31f1eaf2d004c905c0d4181b
Geraint White <geraintwhite@gmail.com>
2015-06-24 17:43:28 +0100

    Add some error case tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -9,6 +9,12 @@ def diff(expected, actual):
 
 class OptionsTests(unittest.TestCase):
 
+    def setUp(self):
+        self.cwd = os.getcwd()
+
+    def tearDown(self):
+        os.chdir(self.cwd)
+
     def test_build_cmd(self):
         self.assertFalse(pr.process(['tests/fixtures/build_cmd']))
         self.assertFalse(diff('tests/expected/build_cmd', 'tests/output/build_cmd'))
@@ -28,6 +34,31 @@ class OptionsTests(unittest.TestCase):
     def test_ignore(self):
         self.assertFalse(pr.process(['tests/fixtures/ignore']))
         self.assertFalse(diff('tests/expected/ignore', 'tests/output/ignore'))
+
+
+class FailureTests(unittest.TestCase):
+
+    def setUp(self):
+        self.cwd = os.getcwd()
+
+    def tearDown(self):
+        os.chdir(self.cwd)
+
+    def test_no_copy_to(self):
+        with self.assertRaises(SystemExit) as cm:
+            pr.process(['tests/fixtures/no_copy_to'])
+        self.assertEqual(cm.exception.code, 'post-receive [error]: no output directory specified in options.json')
+
+    def test_build_cmd_failed(self):
+        with self.assertRaises(SystemExit) as cm:
+            pr.process(['tests/fixtures/build_cmd_failed'])
+        self.assertEqual(cm.exception.code, 'post-receive [error]: exit 1 failed')
+
+    def test_start_cmd_failed(self):
+        with self.assertRaises(SystemExit) as cm:
+            pr.process(['tests/fixtures/start_cmd_failed'])
+        self.assertEqual(cm.exception.code, 'post-receive [error]: exit 1 failed')
+        self.assertFalse(diff('tests/expected/start_cmd_failed', 'tests/output/start_cmd_failed'))
 
 
 if __name__ == '__main__':

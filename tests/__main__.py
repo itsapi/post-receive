69761aa3dd573bb4cfd9bd5489497928feefb0e2
Geraint White <geraintwhite@gmail.com>
2015-06-24 14:05:15 +0100

    Assert exit code of pr in tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -7,18 +7,18 @@ def diff(expected, actual):
     return os.system('diff -rq {} {}'.format(expected, actual))
 
 
-class ProcessingTests(unittest.TestCase):
+class OptionsTests(unittest.TestCase):
 
     def test_copy_to(self):
-        pr.process(['tests/fixtures/copy_to'])
+        self.assertFalse(pr.process(['tests/fixtures/copy_to']))
         self.assertFalse(diff('tests/expected/copy_to', 'tests/output/copy_to'))
 
     def test_copy_from(self):
-        pr.process(['tests/fixtures/copy_from'])
+        self.assertFalse(pr.process(['tests/fixtures/copy_from']))
         self.assertFalse(diff('tests/expected/copy_from', 'tests/output/copy_from'))
 
     def test_build_cmd(self):
-        pr.process(['tests/fixtures/build_cmd'])
+        self.assertFalse(pr.process(['tests/fixtures/build_cmd']))
         self.assertFalse(diff('tests/expected/build_cmd', 'tests/output/build_cmd'))
 
 

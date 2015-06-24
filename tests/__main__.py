c59a12aad8319901c2cd7a8416a0c6aeb43429dc
Geraint White <geraintwhite@gmail.com>
2015-06-24 13:55:26 +0100

    Add copy_from and build_cmd tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -9,9 +9,17 @@ def diff(expected, actual):
 
 class ProcessingTests(unittest.TestCase):
 
-    def test_copy(self):
-        pr.process(['tests/fixtures/copy'])
-        self.assertFalse(diff('tests/expected/copy', 'tests/output/copy'))
+    def test_copy_to(self):
+        pr.process(['tests/fixtures/copy_to'])
+        self.assertFalse(diff('tests/expected/copy_to', 'tests/output/copy_to'))
+
+    def test_copy_from(self):
+        pr.process(['tests/fixtures/copy_from'])
+        self.assertFalse(diff('tests/expected/copy_from', 'tests/output/copy_from'))
+
+    def test_build_cmd(self):
+        pr.process(['tests/fixtures/build_cmd'])
+        self.assertFalse(diff('tests/expected/build_cmd', 'tests/output/build_cmd'))
 
 
 if __name__ == '__main__':

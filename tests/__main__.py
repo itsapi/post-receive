6df7c3cae65a569edc9893e9e6330dd636a54c22
Geraint White <geraintwhite@gmail.com>
2015-06-24 14:38:36 +0100

    Add start_cmd and ignore tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -9,17 +9,25 @@ def diff(expected, actual):
 
 class OptionsTests(unittest.TestCase):
 
-    def test_copy_to(self):
-        self.assertFalse(pr.process(['tests/fixtures/copy_to']))
-        self.assertFalse(diff('tests/expected/copy_to', 'tests/output/copy_to'))
+    def test_build_cmd(self):
+        self.assertFalse(pr.process(['tests/fixtures/build_cmd']))
+        self.assertFalse(diff('tests/expected/build_cmd', 'tests/output/build_cmd'))
 
     def test_copy_from(self):
         self.assertFalse(pr.process(['tests/fixtures/copy_from']))
         self.assertFalse(diff('tests/expected/copy_from', 'tests/output/copy_from'))
 
-    def test_build_cmd(self):
-        self.assertFalse(pr.process(['tests/fixtures/build_cmd']))
-        self.assertFalse(diff('tests/expected/build_cmd', 'tests/output/build_cmd'))
+    def test_copy_to(self):
+        self.assertFalse(pr.process(['tests/fixtures/copy_to']))
+        self.assertFalse(diff('tests/expected/copy_to', 'tests/output/copy_to'))
+
+    def test_start_cmd(self):
+        self.assertFalse(pr.process(['tests/fixtures/start_cmd']))
+        self.assertFalse(diff('tests/expected/start_cmd', 'tests/output/start_cmd'))
+
+    def test_ignore(self):
+        self.assertFalse(pr.process(['tests/fixtures/ignore']))
+        self.assertFalse(diff('tests/expected/ignore', 'tests/output/ignore'))
 
 
 if __name__ == '__main__':

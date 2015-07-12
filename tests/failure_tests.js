b0ae9eeb369c6da097b9f4d4f642871155bb31c9
Geraint White <geraintwhite@gmail.com>
2015-07-12 22:34:04 +0100

    Fix exception throws tests

--- tests/failure_tests.js
+++ tests/failure_tests.js
@@ -5,21 +5,33 @@ var common = require('./common');
 test('no_copy_to', function(t) {
   common.setup('no_copy_to');
   var pr = common.create_pr();
-  t.throws(pr.process, 'post-receive throws an exception');
+
+  t.throws(function() {
+    pr.process();
+  }, 'post-receive throws an exception');
+
   t.end();
 });
 
 test('build_cmd_failed', function(t) {
   common.setup('build_cmd_failed');
   var pr = common.create_pr();
-  t.throws(pr.process, 'post-receive throws an exception');
+
+  t.throws(function() {
+    pr.process();
+  }, 'post-receive throws an exception');
+
   t.end();
 });
 
 test('start_cmd_failed', function(t) {
   common.setup('start_cmd_failed');
   var pr = common.create_pr();
-  t.throws(pr.process, 'post-receive throws an exception');
+
+  t.throws(function() {
+    pr.process();
+  }, 'post-receive throws an exception');
+
   t.notok(common.diff('tests/expected/start_cmd_failed', 'tests/output/start_cmd_failed'), 'output directory is as expected');
   t.end();
 });
